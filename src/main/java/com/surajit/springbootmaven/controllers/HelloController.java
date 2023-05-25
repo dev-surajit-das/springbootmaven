120bebaf533584f4332babfede75415f34a89be2
SurajitD <dev.surajit.das@gmail.com>
2023-05-25 08:01:23 +0530

    master: Added HelloController

---
+++ src/main/java/com/surajit/springbootmaven/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.surajit.springbootmaven.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String printHelloWorld() {
+        return "Hello, World!";
+    }
+}
